a2d6c375c87d027b4e1dc079ec97f62e5c3297b8
sandkur <2decompile1@gmail.com>
2018-11-09 14:16:19 +0700

    "deploy testing"

---
+++ precache-manifest.ce43f271a5c7783e3e204fec44e064ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9c4bba8de4dae18cceaf",
+    "url": "/static/js/main.9c4bba8d.chunk.js"
+  },
+  {
+    "revision": "15d3f388b6ed1eb62f38",
+    "url": "/static/js/1.15d3f388.chunk.js"
+  },
+  {
+    "revision": "9c4bba8de4dae18cceaf",
+    "url": "/static/css/main.da3a45d7.chunk.css"
+  },
+  {
+    "revision": "7a50e9968936df36da9efd4c0083c478",
+    "url": "/index.html"
+  }
+];
